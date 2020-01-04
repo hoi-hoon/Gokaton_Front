65ba3c63e272e686ab66a8fba51e095e76def5c3
dns02023 <dns02023@naver.com>
2020-01-04 13:14:00 +0900

    gg
    gg

---
+++ app/src/main/java/com/example/myapp/mypageUI.java
@@ -0,0 +1,50 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class mypageUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.mypage_layout);
+        Button.OnClickListener onClickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.mypage_searchbtn :
+                        Intent intentA = new Intent(getApplicationContext(), searchUI.class);
+                        startActivity(intentA);
+                        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                        break ;
+                    case R.id.mypage_rankbtn :
+                        Intent intentB = new Intent(getApplicationContext(), rankUI.class);
+                        startActivity(intentB);
+                        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                        break ;
+                    case R.id.mypage_postbtn :
+                        Intent intentC = new Intent(getApplicationContext(), postUI.class);
+                        startActivity(intentC);
+                        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                        break ;
+                }
+            }
+        } ;
+        ImageButton btnsearch = (ImageButton) findViewById(R.id.mypage_searchbtn) ;
+        btnsearch.setOnClickListener(onClickListener) ;
+        ImageButton btnrank = (ImageButton) findViewById(R.id.mypage_rankbtn) ;
+        btnrank.setOnClickListener(onClickListener) ;
+        ImageButton btnpost = (ImageButton) findViewById(R.id.mypage_postbtn) ;
+        btnpost.setOnClickListener(onClickListener) ;
+    }
+}
