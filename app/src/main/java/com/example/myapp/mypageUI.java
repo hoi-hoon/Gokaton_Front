a88857ee724a664075e0cf896f81b20ae95647bf
cjmp1 <chlwjdals1996@gmail.com>
2020-01-05 07:39:12 +0900

    ㅇ

--- app/src/main/java/com/example/myapp/mypageUI.java
+++ app/src/main/java/com/example/myapp/mypageUI.java
@@ -12,12 +12,15 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class mypageUI extends AppCompatActivity {
+    int [] expArray = {0, 144 , 144 , 192 , 240 , 336 , 432 , 528 , 624 , 720 , 816 , 912 , 984,  1056,  1128,  1344,  1440,  1536,  1680,  1824,  1968,  2112,  2208,  2448,  2304,  2496,  2496,  2592,  2688,  3168  };
+    int [] stackArray = {0 , 144 , 288  ,480 , 720  ,1056  ,1488  ,2016 , 2640 , 3360 , 4176 , 5088  ,6072 , 7128,  8256 , 9600,  11040 , 12576 , 14256  ,16080  ,18048 , 20160  ,22368 , 24816 , 27120 , 29616  ,32112 , 34704,  37392 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,8 +101,15 @@ public class mypageUI extends AppCompatActivity {
                 Log.i("받았엉",jObj.toString());
                 String level = jObj.getJSONObject("data").getString("level");
                 String name = jObj.getJSONObject("data").getString("name");
+                String exp = jObj.getJSONObject("data").getString("exp");
                 TextView textView = (TextView)findViewById(R.id.mypage_title);
                 textView.setText("이름 : " +name+" 솔플 레벨 : "+level);
+                textView = (TextView)findViewById(R.id.maxexp);
+                Integer aa = new Integer(expArray[Integer.parseInt(level)]);
+                textView.setText(aa.toString()+"exp");
+                ProgressBar pbar = (ProgressBar)findViewById(R.id.mypage_progress);
+                pbar.setMax(aa);
+                pbar.setProgress(Integer.parseInt(exp) - stackArray[Integer.parseInt(level) - 1]);
 
             } catch (JSONException e) {
                 e.printStackTrace();
--- app/src/main/java/com/example/myapp/postUI.java
+++ app/src/main/java/com/example/myapp/postUI.java
@@ -1,4 +1,11 @@
 package com.example.myapp;
+import android.content.ContentValues;
+import android.database.Cursor;
+import java.io.FileInputStream;
+import java.io.File;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +21,18 @@ import android.widget.ImageView;
 import android.provider.MediaStore;
 import android.widget.EditText;
 import android.text.Editable;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
 import android.view.Gravity;
 import android.text.TextWatcher;
+import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 public class postUI extends AppCompatActivity {
     private final int GET_GALLERY_IMGAGE = 200;
     private ImageView imageview;
@@ -74,6 +88,43 @@ public class postUI extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+
+                SharedPreferences sp = getSharedPreferences("UserTokenKey", MODE_PRIVATE);
+                String uuid = sp.getString("id", "");
+                String token = sp.getString("TokenCode", "");
+                Log.i("시발시발",token);
+                List<String> tagss = new ArrayList<>();
+                String url = "http://52.79.226.55:8000/api/reviews/";
+                final EditText rev_text = findViewById(R.id.review_Text);
+                //a,c,d,b,f,d
+                TextView textView = (TextView)findViewById(R.id.tagA);
+                tagss.add(textView.getText().toString());
+                textView = (TextView)findViewById(R.id.tagC);
+                tagss.add(textView.getText().toString());
+                textView = (TextView)findViewById(R.id.tagD);
+                tagss.add(textView.getText().toString());
+                textView = (TextView)findViewById(R.id.tagB);
+                tagss.add(textView.getText().toString());
+                textView = (TextView)findViewById(R.id.tagF);
+                tagss.add(textView.getText().toString());
+                textView = (TextView)findViewById(R.id.tagE);
+                tagss.add(textView.getText().toString());
+                String rvtext = rev_text.getText().toString();
+
+                ContentValues post_info = new ContentValues();
+
+                post_info.put("userId",uuid);
+                Gson gson = new Gson();
+                String jsonPlace = gson.toJson(tagss);
+                post_info.put("tags",jsonPlace);
+                post_info.put("review",rvtext);
+                Log.i("되라고제발",post_info.toString());
+
+/*
+                NetworkTask networkTask = new NetworkTask(url, post_info);
+                networkTask.execute();
+                */
+
                 // 작성기능
                 Intent intent = new Intent();
                 intent.putExtra("result", "포스팅 완료!");
@@ -91,6 +142,7 @@ public class postUI extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 switch(count_Tag){
+
                     case 0:
                         TextView taga = (TextView)findViewById(R.id.tagA);
                         taga.setText("#"+tagtext.getText().toString());
@@ -129,6 +181,96 @@ public class postUI extends AppCompatActivity {
                 }
             }
         });
+
+    }
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result; // 요청 결과를 저장할 변수.
+            SharedPreferences sp = getSharedPreferences("UserTokenKey", MODE_PRIVATE);
+            String token = sp.getString("TokenCode", "");
+
+            Log.i("갈꺼야",token);
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values,"POST",token);
+
+            try {
+                JSONObject jObj = new JSONObject(result);
+                Log.i("받았엉",jObj.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if(s!=null) {
+                User user = new User();
+                try {
+                    JSONObject jObj = new JSONObject(s);
+                    Log.i("나 갔따왔어",jObj.getJSONObject("data").toString());
+                    SharedPreferences sp = getSharedPreferences("UserTokenKey", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+
+                    editor.putString("id", jObj.getJSONObject("data").getString("id"));
+                    editor.putString("username", jObj.getJSONObject("data").getString("username"));
+                    editor.putString("name", jObj.getJSONObject("data").getString("name"));
+                    editor.putString("email", jObj.getJSONObject("data").getString("email"));
+                    editor.putString("profile", jObj.getJSONObject("data").getString("profile"));
+                    editor.putString("exp", jObj.getJSONObject("data").getString("exp"));
+                    editor.apply();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GET_GALLERY_IMGAGE){
+            sendPicture(data.getData());
+        }
+    }
+    private void sendPicture(Uri imgUri) {
+        Log.e("이미지이미지이미지이미지", imgUri.toString());
+        String imagePath = getRealPathFromURI(imgUri);
+        Log.e("이미지이미지이미지이미지", imagePath);
+
+        File file = new File(imagePath);
+        BufferedImage bufferedImage = ImageIO.read(file);
+        ImageIO.write(bufferedImage, "png", bufferedImage);
+
+        bufferedImage.flush();
+        byte[] imageByte = bufferedImage.toByteArray();
+        bufferedImage.close();
+        return imageByte;
+
+
+
     }
 
+
+    private String getRealPathFromURI(Uri contentUri) {
+
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        cursor.moveToFirst();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        return cursor.getString(column_index);
+    }
 }
